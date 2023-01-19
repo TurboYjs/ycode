2c0430e329ebd8498203825b2585423ecf04c979
guxukai <gxk@33.cn>
2023-01-19 17:28:43 +0800

    chore: complete;

--- server/sh/injectEnv.js
+++ server/sh/injectEnv.js
@@ -3,14 +3,11 @@ const fs = require('fs/promises');
 const args = process.argv.slice(2);
 
 (async function () {
-  let filehandle = null;
   try {
     await fs.unlink('.env');
-    await fs.writeFile('.env', args.join('\n'));
   } catch (err) {
     console.log(err);
-    process.exit(1);
   } finally {
-    await filehandle?.close();
+    await fs.writeFile('.env', args.join('\n'));
   }
 })();
