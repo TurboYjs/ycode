29dd9fbe128ab1509c8d74f0e76ff146b2d84d38
guxukai <gxk@33.cn>
2023-08-15 15:07:01 +0800

    chore: release v0.0.21

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -5,7 +5,7 @@ import App from './App';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 ReactDOM.render(
     <React.StrictMode>
-        <Router basename={'/interview'}>
+        <Router basename={''}>
             <Routes>
                 <Route path="/" element={<App />} />
             </Routes>
